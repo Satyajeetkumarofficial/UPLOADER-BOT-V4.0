bc0b0c44226af85e9519f1689d95cf2804b7a275
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-09-08 13:46:01 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -22,6 +22,7 @@ class Config(object):
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    TG_MAX_FILE_SIZE = int(os.getenv("TG_MAX_FILE_SIZE", 524288000))
     
     OUO_IO_API_KEY = ""
     MAX_MESSAGE_LENGTH = 4096
