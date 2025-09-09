58ea6f23b6fa9c4c5c81d20465dff640ec15d497
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-09-09 16:31:43 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -36,7 +36,7 @@ cookies_file = 'cookies.txt'
 async def echo(bot, update):
     # Step 1: Check banned users
     if await db.is_banned(update.from_user.id):
-    await update.reply_text(
+      await update.reply_text(
         text="🚫 आप इस बॉट का उपयोग नहीं कर सकते।",
         disable_web_page_preview=True
     )
