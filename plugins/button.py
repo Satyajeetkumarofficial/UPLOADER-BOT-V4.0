5274fd7912b2d48e6031fba25c2b12519d6259fc
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-09-09 20:13:20 +0530

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -301,8 +301,8 @@ async def youtube_dl_call_back(bot, update):
             duration=duration,
             length=width,
             thumb=thumbnail
-        )
-        )
+      )
+      )
 
         log_caption = f"📥 Uploaded by: {update.from_user.mention}\nFile Name: {custom_file_name}\nSize: {humanbytes(os.path.getsize(download_directory))}"
         await bot.send_video_note(
